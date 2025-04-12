1b1e09919d42eca02be84e5c379d9a5cc168fb4c
Moyan <linmoyan0111@gmail.com>
2025-04-12 16:03:32 -0700

    Initial Commit

---
+++ DogPhotoUploader/DogPhotoUploaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogPhotoUploaderApp.swift
+//  DogPhotoUploader
+//
+//  Created by Moyan on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DogPhotoUploaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DogPhotoUploaderTests/DogPhotoUploaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  DogPhotoUploaderTests.swift
+//  DogPhotoUploaderTests
+//
+//  Created by Moyan on 4/12/25.
+//
+
+import Testing
+@testable import DogPhotoUploader
+
+struct DogPhotoUploaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
